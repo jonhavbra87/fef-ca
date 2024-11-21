89a3a67ebd9df18544078079abea8cb821eab742
Jon Are <jon.are.br@gmail.com>
2024-11-21 12:55:39 +0100

    header, footer, nav, & layout added. working navigation and simple styling added. Also created types/images.d.ts for jpg, png & svg. changed tsconfig files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
+import "./index.css"
+
+import { Routes, Route } from "react-router-dom"
+import Layout from "./components/Layout";
+
+
+function Home() {
+  return <div className="flex-grow">Home</div>
+}
+
+function Products() {
+  return <div>Products</div>
+}
+
+
+function RouteNotFound() {
+  return <div>Page not found</div>
+}
+
 
 
 function App() {
-
-
-  return (
-
-    <div>
-   <h1>Hello World</h1>
-  </div>
-  )
-}
+    return (
+      <div className="h-screen">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="products" element={<Products />} />
+            <Route path="*" element={<RouteNotFound />} />
+          </Route>
+        </Routes>
+      </div>
+    )
+  }
 
 export default App
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+    <div>
+        <footer className="bg-blue-700 bottom-0 text-white fixed w-full p-5 text-center">
+            <p>This is the Footer</p>
+        </footer>
+    </div>
+    )
+}
+
+export default Footer
